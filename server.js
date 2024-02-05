6644f56c985d10d7568990e119bd2a35adcc1cbd
Marline John <marlinejohn@gmail.com>
2024-02-05 12:38:34 +0100

    Created server.js and imported http module

---
+++ server.js
@@ -0,0 +1,8 @@
+let http = require('http');
+
+http.createServer((request, reponse) =>{
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.end('Hello node !\n');
+}).listen(8080)
+
+console.log('My first Node test server is running on Port 8080.');
