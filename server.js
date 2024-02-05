f36725f8c6d829493d3a12b5cd8fffda4b249c9d
Marline John <marlinejohn@gmail.com>
2024-02-05 12:44:19 +0100

    Corrected a typing error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let http = require('http');
 
-http.createServer((request, reponse) =>{
+http.createServer((request, response) =>{
     response.writeHead(200, {'Content-Type' : 'text/plain'});
     response.end('Hello node !\n');
 }).listen(8080)
