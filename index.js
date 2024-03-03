79baec4cc066bed7f1de8e2dad1fee34f9817405
Marline John <marlinejohn@gmail.com>
2024-03-03 23:56:06 +0100

    Add new mongoose.connect function

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ const Genres = Models.Genre;
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect("mongodb://localhost:27017/myMoviesDb", {
+// mongoose.connect("mongodb://localhost:27017/myMoviesDb", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
